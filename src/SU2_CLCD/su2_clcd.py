13aab0be72233da07a9c1ccd5392649f55d968a7
aniket <aniket@stanford.edu>
2013-10-08 20:11:50 -0700

    added mesh and config file

--- src/SU2_CLCD/su2_clcd.py
+++ src/SU2_CLCD/su2_clcd.py
@@ -6,7 +6,7 @@ from scipy.interpolate import interp1d
 # OpenMDAO imports
 from openmdao.lib.datatypes.api import Float, Int, Array, VarTree
 from openmdao.main.api import Component, Assembly
-from SU2_wrapper import Solve
+from SU2_wrapper import Solve, Deform
 
 # SU2 imports
 from SU2.io import Config
@@ -22,7 +22,7 @@ class SU2_CLCD(Solve):
   coefficientOfDrag = Float(iotype="out", desc="coefficient of lift from the airfoil section at that angle of attack")
 
   def __init__(self):
-    print "SU2_CLCD item created. config_in=", self.config_in
+    super(Solve,self).__init__()
 
   def execute(self):
     if self.kind == "Fake_SU2":
@@ -57,28 +57,47 @@ class SU2_CLCD_Sections(Assembly):
 
   def configure(self):
     for i in range(self.nElems):
-      su2comp = "SU2_%d"%i
-      self.add(su2comp, SU2_CLCD())
-      self.connect("alphas[%d]"%i, su2comp+".alpha")
-      self.connect(su2comp+".coefficientOfLift","cls[%d]"%i)
-      self.connect(su2comp+".coefficientOfDrag", "cds[%d]"%i)
-      self.driver.workflow.add(su2comp)
+
+      # Create deform and solve objects
+      su2def   = "SU2_deform_%d"%i
+      su2solve = "SU2_solve_%d"%i
+      self.add(su2def  , Deform())
+      self.add(su2solve, SU2_CLCD())
+
+      # TODO: Clean this up
+      new_config = Config()
+      new_config.read('inv_NACA0012.cfg') 
+      exec(compile('self.%s.config_in=new_config'%su2def,'<string>','exec'))
+
+      # Connect deform and solve objects together
+      self.connect('self.%s.mesh_file' %su2def, '%s.mesh_file'%su2solve)
+      self.connect('%s.config_out'%su2def, '%s.config_in'%su2solve)
+
+      # Connect this assembly to solve objects
+      self.connect("alphas[%d]"%i, su2solve+".alpha")
+      self.connect(su2solve+".coefficientOfLift","cls[%d]"%i)
+      self.connect(su2solve+".coefficientOfDrag", "cds[%d]"%i)
+
+      # Add both deform and solve to the workflow
+      self.driver.workflow.add(su2def)
+      self.driver.workflow.add(su2solve)
 
   def set_su2_config(self, filename):
     '''Routine to specify an SU^2 config file'''
     for i in range(self.nElems):
 
-      # Name of the i'th su2 object
-      su2comp = "SU2_%d"%i
+      # Name of the i'th su2 deform object
+      su2def = "SU2_deform_%d"%i
 
       # Create a new config option and read the file into it
       new_config = Config()
       new_config.read(filename)
 
-      # Using eval to deal with su2comp variable
-      evalcmd = 'self.%s.config_in=%s'%(su2comp,new_config)
-      print evalcmd
-      eval(evalcmd)
+      # Using eval to deal with su2def variable
+
+      execcmd = compile('self.%s.config_in=new_config'%(su2def))
+      print execcmd
+      exec(execmd)
 
 
 
