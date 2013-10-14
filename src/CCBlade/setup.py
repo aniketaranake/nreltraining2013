b1f061ac9c9e49b1988a2a467186e60d8903df0b
btracey <bdtinternet@gmail.com>
2013-10-14 16:20:29 -0700

    Added ccblade setup

---
+++ src/CCBlade/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from numpy.distutils.core import setup, Extension
+
+
+
+
+setup(
+    name='CCBlade',
+    version='1.0.0',
+    description='Blade element momentum aerodynamics for wind turbines',
+    author='S. Andrew Ning',
+    author_email='andrew.ning@nrel.gov',
+    package_dir={'': 'src'},
+    py_modules=['ccblade', 'airfoilprep'],
+    # install_requires=['numpy', 'scipy'],
+    # test_suite='test.test_ccblade.py',
+    license='Apache License, Version 2.0',
+    ext_modules=[Extension('_bem', ['src/bem.f90'], extra_compile_args=['-O2'])]
+)
+
