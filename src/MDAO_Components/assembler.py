59ba86c3d6ef289415d34d04e75697a30ef718b2
btracey <bdtinternet@gmail.com>
2013-10-14 20:19:12 -0700

    Fixed a bunch of errors in assembler.py

--- src/MDAO_Components/assembler.py
+++ src/MDAO_Components/assembler.py
@@ -9,9 +9,9 @@ from su2_caller import SU2_CLCD_Fake
 # OpenMDAO imports
 from openmdao.main.api import Component, Assembly, VariableTree
 from openmdao.lib.datatypes.api import Float, Int, Array, VarTree
-#from openmdao.lib.drivers.api import SLSQPdriver, COBYLAdriver, CONMINdriver
+from openmdao.lib.drivers.api import SLSQPdriver, COBYLAdriver, CONMINdriver
 
-from custom_opt import SLSQPdriver
+#from custom_opt import SLSQPdriver
 
 from openmdao.lib.casehandlers.api import DumpCaseRecorder
 
@@ -25,21 +25,20 @@ class blade_opt(Assembly):
 	nSweep = 10
 	nElements = 12
 	def configure(self):
-		self.add('su2',SU2_CLCD_Fake(nSweep=nSweep))
-		self.add('bem',nElements=nElements)
+		self.add('su2',SU2_CLCD_Fake(nSweep=self.nSweep))
+		self.add('bem',BEMComponent(n_elements=self.nElements,nSweep=self.nSweep))
 		self.add('driver',SLSQPdriver())
 		self.driver.workflow.add(['bem','su2'])
 
-		for i in range(nElements){
+		for i in range(self.nElements):
 			self.driver.add_parameter('bem.theta[%d]'%i)
 			self.driver.add_parameter('bem.chord[%d]'%i)
 			self.connect('su2.cls[%d]'%i, 'bem.cls[%d]'%i)
 			self.connect('su2.alphas[%d]'%i, 'bem.alphas[%d]'%i)
 			self.connect('su2.cds[%d]'%i, 'bem.cds[%d]'%i)
-		}
 		self.driver.add_objective('bem.power')
 
 if __name__=="__main__":
 	bo = blade_opt()
-    bo.run()
-    print bo.driver.error_code
+	bo.run()
+	print bo.driver.error_code
