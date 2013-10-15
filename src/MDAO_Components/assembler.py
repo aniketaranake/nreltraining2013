16a93f88216bdee11ba96e0bcf371f0bf59f6e90
Aniket C. Aranake <aniketaranake@gmail.com>
2013-10-15 11:40:10 -0700

    Justin's updated assembler.py

--- src/MDAO_Components/assembler.py
+++ src/MDAO_Components/assembler.py
@@ -22,26 +22,27 @@ from SU2.io import Config
 
 class blade_opt(Assembly):
 
-	nSweep = 10
-	nElements = 17
-	def configure(self):
-		self.add('su2',SU2_CLCD_Fake(nSweep=self.nSweep))
-		self.add('bem',BEMComponent(n_elements=self.nElements,nSweep=self.nSweep))
-		self.add('driver',SLSQPdriver())
-		self.driver.workflow.add(['bem','su2'])
+  nSweep = 10
+  nElements = 17
+  def configure(self):
+    self.add('su2',SU2_CLCD_Fake(nSweep=self.nSweep))
+    self.add('bem',BEMComponent(n_elements=self.nElements,nSweep=self.nSweep))
+    self.add('driver',SLSQPdriver())
+    self.driver.workflow.add(['bem','su2'])
 
-		for i in range(self.nElements):
-			self.driver.add_parameter('bem.theta[%d]'%i,low=-80,high=80)
-			self.driver.add_parameter('bem.chord[%d]'%i,low=1e-8,high=10,start=1)
-		for i in range(self.nSweep):
-			self.connect('su2.cls[%d]'%i, 'bem.cls[%d]'%i)
-			self.connect('su2.alphas[%d]'%i, 'bem.alphas[%d]'%i)
-			self.connect('su2.cds[%d]'%i, 'bem.cds[%d]'%i)
-		self.driver.add_objective('-bem.power')
-		self.driver.max_iter = 1000
+    for i in range(self.nElements):
+      self.driver.add_parameter('bem.theta[%d]'%i,low=-80,high=80)
+      self.driver.add_parameter('bem.chord[%d]'%i,low=1e-8,high=10,start=1)
+    for i in range(self.nSweep):
+      self.connect('su2.cls[%d]'%i, 'bem.cls[%d]'%i)
+      self.connect('su2.alphas[%d]'%i, 'bem.alphas[%d]'%i)
+      self.connect('su2.cds[%d]'%i, 'bem.cds[%d]'%i)
+    self.driver.add_objective('-bem.power')
+    self.driver.max_iter = 1000
 
 if __name__=="__main__":
-	bo = blade_opt()
-	bo.run()
-	print bo.driver.error_code
-	#print bo.driver.__dict__
+  bo = blade_opt()
+  bo.run()
+  print bo.driver.error_code
+  #print bo.driver.__dict__
+
